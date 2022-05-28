6a19a5a80d118aae36df637053fe93412d72ad91
M4TY <matyhimmer@gmail.com>
2022-05-28 22:11:40 +0200

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let name = "world";
+</script>
+
+<main>
+    <h1>Hello {name}, welcome to NetPioneer template!</h1>
+</main>
+
+
+<style lang="scss">
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+
+        h1 {
+            font-family: Roboto,serif;
+            font-weight: lighter;
+            font-size: 2em;
+            color: #de5252;
+            margin-top: 20px;
+        }
+    }
+</style>
+
